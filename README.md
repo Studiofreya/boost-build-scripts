ae197b7efffc6dc26a99f6917182c9c691784469
Kent <kent@studiofreya.com>
2015-12-19 21:59:52 +0100

    Initial commit with scripts for building Boost 1.58, 1.59 and 1.60 with Visual Studio 2013 and Visual Studio 2015

---
+++ README.md
@@ -0,0 +1,9 @@
+# Scripts for building Boost for Visual Studio on Windows
+
+Extract Boost into this folder and run the script for your version of Boost and Visual Studio.
+
+Both 32-bit and 64-bit targets will be built.
+
+Compiled libraries will be placed in the **boost_{version}/stage/{platform}/lib** directory.
+
+A guide on how to add Boost to your projects is available at http://studiofreya.com/
---
+++ build_boost_1_58_vs2013.bat
@@ -0,0 +1,10 @@
+call "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat" x86
+
+cd boost_1_58_0
+call bootstrap.bat
+
+rem Most libraries can be static libs
+b2 -j8 toolset=msvc-12.0 address-model=64 architecture=x86 link=static threading=multi runtime-link=shared --build-type=minimal stage --stagedir=stage/x64 
+b2 -j8 toolset=msvc-12.0 address-model=32 architecture=x86 link=static threading=multi runtime-link=shared --build-type=minimal stage --stagedir=stage/win32
+
+pause
